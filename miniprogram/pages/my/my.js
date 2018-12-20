c73fe1c5eac3e53cadd82b985f82faa15a1db5e9
zl <zljxzs@163.com>
2018-12-20 10:38:26 +0800

    20181220

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -5,62 +5,83 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    array: [{
+      id: 1,
+      name: 1
+    }, {
+      id: 2,
+      name: 2
+    }, {
+      id: 3,
+      name: 3
+    }, {
+      id: 4,
+      name: 4
+    }, {
+      id: 5,
+      name: 5
+    }, {
+      id: 6,
+      name: 6
+    }, {
+      id: 7,
+      name: 7
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
